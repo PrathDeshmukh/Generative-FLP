ce0fc54d8521bdcbaf3fbe9708e52525365a3434
PrathDeshmukh <prathampushparaj.deshmukh@epfl.ch>
2025-04-23 13:38:40 +0200

    Modifications to utilize utils.py

--- src/tsne_plotter.py
+++ src/tsne_plotter.py
@@ -3,43 +3,12 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import rdkit.Chem
 import seaborn as sns
 from matplotlib.pyplot import tight_layout
 from sklearn.manifold import TSNE
 
+from utils.utils import calcfps
 
-from rdkit.Chem.rdmolfiles import MolFromSmiles
-from rdkit.Chem import rdFingerprintGenerator
-
-def moltoFPS(mol: rdkit.Chem.Mol):
-    try:
-        fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
-        fps1 = fpgen.GetFingerprint(mol)
-        return list(fps1)
-
-    except:
-        print('Exception!')
-
-def smilestoFPS(smiles_csv) -> list:
-    df = pd.read_csv(smiles_csv)
-    smiles = df['SMILES']
-    fps2 = []
-    for smi in smiles:
-        try:
-            mol = MolFromSmiles(smi)
-            fp = moltoFPS(mol)
-            if fp is not None:
-                fps2.append(fp)
-        except:
-            print('Exception')
-    return fps2
-
-def inputtoFPS(input_path: str, fps_dict: dict):
-    name = os.path.basename(input_path).split('.')[0]
-    fps_ = np.array(smilestoFPS(input_path))
-    fps_dict[name] = fps_
-    return fps_dict
 
 if __name__ == '__main__':
     def parse_cmd():
@@ -56,19 +25,22 @@ if __name__ == '__main__':
     file_paths = cmd_args.file_paths
 
     n_files = len(file_paths)
+    perplexity = 60
 
     if n_files != 0:
         fps_dict = {}
         for file in file_paths:
-            fps_dict = inputtoFPS(file, fps_dict)
-            print(f'{file} done')
+            fname = os.path.basename(file).split('.')[0]
+            fps_list = calcfps(file)
+            fps_dict[fname] = np.array(fps_list)
+            print(f'Fingerprints for {fname} calculated!')
 
         fig, ax = plt.subplots(1,1, figsize=(12,12), tight_layout=True)
         palette = sns.color_palette("bright")
         label_colors = dict(zip(fps_dict.keys(), palette))
 
         tsne = TSNE(n_components=2,
-                    perplexity=60,
+                    perplexity=perplexity,
                     init="pca",
                     random_state=42)
 
@@ -79,7 +51,7 @@ if __name__ == '__main__':
             plt.legend()
 
         plt.title("t-SNE visualization of various datasets")
-        plt.savefig('./test-tsne.png')
+        plt.savefig(f'./Generative-FLP/data/tsne_p{perplexity}.png')
         print("Plot generated!")
 
     else:
