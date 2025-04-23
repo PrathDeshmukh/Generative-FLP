3ad3cca2a3c0e60f0173e14a7d1994cdfba8a031
PrathDeshmukh <prathampushparaj.deshmukh@epfl.ch>
2025-04-23 13:42:15 +0200

    Modifications to make histograms with density distribution

--- src/dataset_diagnostics.py
+++ src/dataset_diagnostics.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.pyplot import tight_layout
 from rdkit.Chem import rdMolDescriptors as rdmd
 from rdkit.Chem.rdmolfiles import MolFromSmiles
+import argparse
 
-import matplotlib.pyplot as plt
 
 #Molecule properties to be calculated no. of heavy atoms, heter atoms, aromatic rings, aliphatic rings, CalcFractionCSP3, VDWvolume
 
-path = '/home/pratham/PycharmProjects/ga_flp_ruben/Literature-SMILES.csv'
+def parse_cmd():
+    parser = argparse.ArgumentParser(description="Generate t-SNE plots of various databases")
+    parser.add_argument(
+        "--file_paths",
+        nargs='*')
+    return parser.parse_args()
 
-df = pd.read_csv(path)
-smiles = np.array(df['SMILES'].values)
-
+#dictionary of properties to be calculated
 properties = {
     'nha' :rdmd.CalcNumHeavyAtoms,
     'nhta':rdmd.CalcNumHeteroatoms,
@@ -21,30 +28,77 @@ properties = {
     #'vdwv':[]
 }
 
-prop_dict = {key: [] for key in properties.keys()}
+#property names for plots
+prop_names = {
+    'nha' :'Num Heavy atoms',
+    'nhta':'Num Hetero atoms',
+    'nar' :'Num Aromatic Rings',
+    'nalr':'Num Aliphatic Rings',
+    'fsp3':'Fraction CSP3'
+}
 
+#function to calculate all the properties for a given SMILES string
 def calc_props(smi: str):
     try:
         mol = MolFromSmiles(smi)
         if mol is not None:
             mol.UpdatePropertyCache()
             return {prop: func(mol) for prop, func in properties.items()}
+
     except Exception as e:
-        print(f"Exception {e} in SMILES: {smi}")
+        print(f"[!] Error processing SMILES {smi}: {e}")
+        return None
 
-for smi in smiles:
-    props = calc_props(smi)
-    print(props)
-    if props:
-        for prop, val in props.items():
-            prop_dict[prop].append(val)
+def main():
+    cmd_args = parse_cmd()
+    file_paths = cmd_args.file_paths
+    if not file_paths:
+        print("[!] no files provided.")
+        return
 
+    all_props_dict = {}
 
-fig, axs = plt.subplots(3,2)
-axes = axs.flatten()
+    for file in file_paths:
+        df = pd.read_csv(file)
+        smiles = df['SMILES']
+        dataset_name = os.path.basename(file).split('.')[0]
 
-for prop, ax in zip(prop_dict.keys(), axes):
-    ax.hist(prop_dict[prop])
-    ax.set_title(f'{prop}')
+        prop_dict = {key: [] for key in properties.keys()}
 
-plt.show()
+        for smi in smiles:
+            props = calc_props(smi)
+            if props is not None:
+                for prop, val in props.items():
+                    prop_dict[prop].append(val)
+
+        all_props_dict[f'{dataset_name}'] = prop_dict
+
+    #plotting histograms
+    fig, axs = plt.subplots(3,2, figsize=(15,15), tight_layout=True)
+    axes = axs.flatten()
+    fig.delaxes(axes[-1]) #remove unused sixth plot
+
+    palette = sns.color_palette("bright")
+    label_colors = dict(zip(all_props_dict.keys(), palette))
+
+    for file in all_props_dict.keys():
+        for prop, ax in zip(all_props_dict[file].keys(), axes):
+            sns.histplot(data=all_props_dict[file],
+                         x=all_props_dict[file][prop],
+                         ax=ax,
+                         stat='density',
+                         color=label_colors[file])
+
+            ax.set_title(f'{prop_names[prop]}')
+
+    fig.legend(all_props_dict.keys(),
+               loc='upper center',
+               ncol=len(all_props_dict),
+               frameon=False)
+
+    plt.tight_layout(rect=[0.0, 0.0, 1.0, 0.95])
+    plt.savefig('./data/properties_histogram.png')
+    print("Plot generated!")
+
+if __name__=='__main__':
+    main()
