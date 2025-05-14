44f91dcb69ee6194aacbf5a31eae0842bbfd8f4b
PrathDeshmukh <prathampushparaj.deshmukh@epfl.ch>
2025-05-14 17:59:02 +0200

    added outlier detection and removal as final step

--- src/fragmentation.py
+++ src/fragmentation.py
@@ -2,11 +2,13 @@
 from rdkit import Chem
 from rdkit.Chem import rdRGroupDecomposition
 import pandas as pd
+from rdkit.Chem import rdMolDescriptors as rdmd
+import numpy as np
 
-scaffold_csv = '/home/pratham/Desktop/data/scaffold_smiles.csv'
+scaffold_csv = '/home/ppdeshmu/Generative-FLP/data/csv/scaffold_smiles_labelled.csv'
 scaffold_df = pd.read_csv(scaffold_csv)
 
-oscar_csv = '/home/pratham/Desktop/data/OSCAR_SMILES_all.csv'
+oscar_csv = '/home/ppdeshmu/Generative-FLP/data/csv/OSCAR_SMILES_all.csv'
 oscar_df = pd.read_csv(oscar_csv)
 
 scaf_mol_list = [Chem.MolFromSmiles(scaffold) for scaffold in scaffold_df['SMILES']]
@@ -31,18 +33,52 @@ group_set = set(groups_all)
 
 #Remove duplicates by comparing mol objects
 subs_smiles = list(group_set)
+scaf_og_csv = '/home/ppdeshmu/oscar_extraction/scaffold_smiles.csv'
+scaf_og_df = pd.read_csv(scaf_og_csv)
+scaf_og_mols = [Chem.MolFromSmiles(smi) for smi in scaf_og_df['SMILES']]
 
 removed_smiles = []
+kept_smiles = []
+
 for smi in subs_smiles:
     subs_mol = Chem.MolFromSmiles(smi)
-    for scaf_mol in scaf_mol_list:
+    matched = False
+    for scaf_mol in scaf_og_mols:
         if subs_mol.HasSubstructMatch(scaf_mol):
             removed_smiles.append(smi)
-            subs_smiles.remove(smi)
+            matched = True
             break
+    if not matched:
+        kept_smiles.append(smi)
+
+
+#Removing outliers structures on the basis of molecular size by iqr method
+nha_list = []
+for smi in kept_smiles:
+    mol = Chem.MolFromSmiles(smi)
+    nha = rdmd.CalcNumHeavyAtoms(mol)
+    nha_list.append(nha)
+
+q1 = np.quantile(nha_list, 0.25)
+q3 = np.quantile(nha_list, 0.75)
+median = np.median(nha_list)
+
+iqr = q3 - q1
+
+upper_bound = q3 + 1.5*iqr
+lower_bound = q1 - 1.5*iqr
+
+cleaned_kept_smiles = []
+for i, nha in enumerate(nha_list):
+    if lower_bound < nha < upper_bound:
+        cleaned_kept_smiles.append(kept_smiles[i])
+    else:
+        removed_smiles.append(kept_smiles[i])
+
+print(len(cleaned_kept_smiles))
 
 removed_df = pd.DataFrame({'SMILES':removed_smiles})
 removed_df.to_csv('/home/ppdeshmu/Generative-FLP/data/csv/Removed_smiles.csv')
 
-df = pd.DataFrame({'SMILES': subs_smiles})
+df = pd.DataFrame({'SMILES': cleaned_kept_smiles})
 df.to_csv('/home/ppdeshmu/Generative-FLP/data/csv/Substituents_smiles.csv')
