80cb585b1675664fb69be58e1baf0d93100dfc4e
PrathDeshmukh <prathampushparaj.deshmukh@epfl.ch>
2025-06-19 18:57:42 +0200

    added function to calculate geometrical descriptors

--- src/calc_utils.py
+++ src/calc_utils.py
@@ -6,11 +6,12 @@ from xtb.ase.calculator import XTB
 from ase.optimize import BFGS
 import numpy as np
 import multiprocessing
-import subprocess as sp
 from rdkit import Chem, RDLogger
-from rdkit.Chem import AllChem
-from rdkit.Chem.rdmolfiles import MolToSmiles as mol2smi
-
+from typing import List, Tuple
+from scipy.spatial import distance
+from skspatial.objects import Plane, Points
+import ase
+from io import StringIO
 
 logger = logging.getLogger(__name__)
 lg = RDLogger.logger()
@@ -52,6 +53,11 @@ def get_dft_E(xyz, label, charge:int = 0):
 
     return energy
 
+def atoms_to_xyz_str(atoms: ase.Atoms):
+  f = StringIO()
+  atoms.write(f, format="xyz")
+  return f.getvalue()
+
 def unit_vector(v):
     return v / np.linalg.norm(v)
 
@@ -60,6 +66,27 @@ def angle_between(u, v):
     u_1 = unit_vector(u)
     return np.arccos(np.clip(np.dot(v_1, u_1), -1.0, 1.0))
 
+def calc_directed_vector(lewis_idx, subs_idx, coords):
+    subs_coords = [coords[i] for i in subs_idx]
+    vec = 0.0
+    if len(subs_coords) == 2:
+        subs_coords.append(lewis_idx)
+        points = Points(subs_coords)
+        try:
+            centroid = points.centroid()
+            vec = np.array(coords[lewis_idx]) - centroid
+        except ValueError:
+            return 0.0
+
+    elif len(subs_coords) == 3:
+        points = Points(subs_coords)
+        try:
+            centroid = points.centroid()
+            vec = np.array(coords[lewis_idx]) - centroid
+        except ValueError:
+            return 0.0
+
+    return vec
 
 def xtb_opt_(xyz, save_folder, charge: int):
     filename = os.path.basename(xyz)
@@ -81,6 +108,268 @@ def get_lewis_h_dist(xyz, lewis_idx):
 
     dist = np.linalg.norm(lewis_pos - H_pos)
     return dist
+
+def calculate_angle(
+    B_idx: int, N_idx: int, cm: np.ndarray, coords: np.ndarray, verb: int = 0
+) -> float:
+    """
+    Calculate the angle between two vectors for a given set of atoms.
+
+    Parameters:
+    - B_idx (int): Index of the boron atom.
+    - N_idx (int): Index of the nitrogen atom.
+    - cm (np.ndarray): Adjacency matrix.
+    - coords (np.ndarray): Atomic coordinates.
+    - verb (int): Verbosity level.
+
+    Returns:
+    float
+        The angle in degrees between two vectors, or 0.0 if the calculation cannot be performed.
+    """
+    subs_base = []
+    for n, j in enumerate(cm[N_idx, :]):
+        if j == 1:
+            pos = coords[n]
+            pos = np.around(pos, 4)
+            subs_base.append(pos)
+    if len(subs_base) == 2:
+        if verb > 1:
+            print("\nsp2 base. Calculating planes.")
+        subs_base.append(coords[N_idx, :])
+        points = Points(subs_base)
+        try:
+            centroid = points.centroid()
+        except ValueError:
+            if verb > 0:
+                print("Cannot calculate centroid. return 0.0")
+            return 0.0
+        vec_b = np.array(coords[N_idx, :]) - centroid
+
+    elif len(subs_base) == 3:
+        if verb > 1:
+            print("\nsp3 base. Calculating planes.")
+        points = Points(subs_base)
+        try:
+            centroid = points.centroid()
+        except ValueError:
+            if verb > 0:
+                print("Cannot calculate centroid. return 0.0")
+            return 0.0
+        vec_b = np.array(coords[N_idx, :]) - centroid
+    else:
+        print(
+            f"Base substituents are not 2 or 3 but rather {len(subs_base)} Angle set to 0!"
+        )
+        return 0.0
+
+    vec_b = unit_vector(vec_b)
+    subs_acid = []
+    for n, j in enumerate(cm[B_idx, :]):
+        if j == 1:
+            pos = coords[n]
+            pos = np.around(pos, 4)
+            subs_acid.append(pos)
+
+    if len(subs_acid) == 3:
+        if verb > 1:
+            print("\nsp3 acid. Calculating planes.")
+
+        points = Points(subs_acid)
+        try:
+            centroid = points.centroid()
+        except ValueError:
+            return 0.0
+        vec_a = np.array(coords[B_idx, :]) - centroid
+    else:
+        if verb > 0:
+            print("Acid substituents are not 3!")
+        return 0.0
+
+    vec_a = unit_vector(vec_a)
+
+    theta = angle_between(vec_a, vec_b) * 180 / np.pi
+    return theta
+
+
+def get_shortest_BN_distance(
+    mol: Chem.Mol, coords: np.ndarray, threshold: float
+) -> Tuple[float|None, int|None, int|None]:
+    """
+    Calculate the shortest B-N distance above a specified threshold.
+
+    Parameters:
+    - mol (Chem.Mol): The molecular structure.
+    - coords (np.ndarray): The coordinates of all atoms in the molecule.
+    - threshold (float): The threshold for considering B-N distances.
+
+    Returns:
+    tuple: A tuple containing the following:
+        - float or None: The shortest B-N distance greater than the threshold, or None if no such distance is found.
+        - int or None: The index of the boron (B) atom corresponding to the shortest distance, or None if not found.
+        - int or None: The index of the nitrogen (N) atom corresponding to the shortest distance, or None if not found.
+    """
+    B_dict = {}
+    N_dict = {}
+
+    for atom, c in zip(mol.GetAtoms(), coords):
+        if atom.GetAtomicNum() == 5:
+            B_dict[atom.GetIdx()] = c
+        elif atom.GetAtomicNum() == 7:
+            N_dict[atom.GetIdx()] = c
+
+    BN_dist_idx = {}
+
+    for bidx, bpos in B_dict.items():
+        for nidx, npos in N_dict.items():
+            dist = np.linalg.norm(bpos - npos)
+            if dist > threshold:
+                BN_dist_idx[dist] = (bidx, nidx)
+            else:
+                continue
+
+    min_dist = min(BN_dist_idx.keys())
+    if min_dist:
+        B_idx, N_idx = BN_dist_idx[min_dist]
+        return min_dist, B_idx, N_idx
+    else:
+        return None, None, None
+
+
+def get_H2_pos(
+        z: List[int],
+        B_idx: int,
+        N_idx: int,
+        cm: np.ndarray,
+        coords: np.ndarray,
+        verb: int = 0,
+) -> Tuple[np.ndarray, np.ndarray, List[int], List[int]]:
+    """
+    Determine cartesian coodinates to which hydride and proton are to be added.
+
+    Vector definition on N: The centroid point of substituent groups - Coordinate of N
+    Vector definition on B: Coordinate of H(N) - Coordinate of B
+
+    Set bond distance:
+    d(B-H) = 1.2
+    d(N-H) = 1.2
+
+    Parameters:
+    1. z: vector of atomic numbers
+    2. N_idx: atomic index of N
+    3. B_idx: atomic index of B
+    4. cm: connectivity matrix
+    5. coords: coordinates matrix
+    6. verb: verbosity level
+
+    Returns:
+    H_a, H_b: coordinate of H(B) and H(N), respectively
+    subs_acid_idx, subs_base_idx: Indexes of atoms around the respective Lewis centers
+    """
+
+    # base vector definition
+    subs_base = []
+    subs_base_idx = []
+    for n, j in enumerate(cm[N_idx, :]):
+        if j == 1 and z[n] != 5:
+            subs_base.append(np.around(coords[n], 4))
+            subs_base_idx.append(n)
+
+    if len(subs_base) == 2:
+        if verb > 1:
+            print("Detect sp2 Nitrogen\n")
+        subs_base.append(coords[N_idx, :])
+        points = Points(subs_base)
+        try:
+            centroid = points.centroid()
+            vec_b = np.array(coords[N_idx, :]) - centroid
+        except ValueError:
+            if verb > 0:
+                print("Cannot calculate centroid. return None")
+            return None, None, None, None
+
+    elif len(subs_base) == 3:
+        if verb > 1:
+            print("Detect sp3 nitrogen\n")
+        points = Points(subs_base)
+
+        try:
+            plane = Plane.best_fit(points)
+            n1 = plane.normal
+            n2 = -n1
+            H_b1 = n1 * 1.2
+            H_b2 = n2 * 1.2
+            d1 = distance.euclidean(H_b1, coords[B_idx, :])
+            d2 = distance.euclidean(H_b2, coords[B_idx, :])
+            if d1 < d2:
+                vec_b = H_b1
+            else:
+                vec_b = H_b2
+
+
+        except ValueError:
+            if verb > 0:
+                print("Cannot determine the base vector. return None")
+            return None, None, None, None
+
+    else:
+        print(
+            f"Base substituents are not 2 or 3 but rather {len(subs_base)} return None"
+        )
+        return None, None, None, None
+
+    vec_b = unit_vector(vec_b)
+    H_b = vec_b * 1.2 + coords[N_idx, :]
+
+    # Acid vector definition
+    subs_acid = []
+    subs_acid_idx = []
+    for n, j in enumerate(cm[B_idx, :]):
+        if j == 1 and z[n] != 7:
+            subs_acid.append(np.around(coords[n], 4))
+            subs_acid_idx.append(n)
+
+    if len(subs_acid) == 2:
+        if verb > 1:
+            print("Detect sp2 Boron\n")
+        subs_acid.append(coords[B_idx, :])
+        points = Points(subs_acid)
+        try:
+            centroid = points.centroid()
+            vec_a = np.array(coords[B_idx, :]) - centroid
+        except ValueError:
+            if verb > 0:
+                print("Cannot calculate centroid. return None")
+            return None, None, None, None
+
+    elif len(subs_acid) == 3:
+        if verb > 1:
+            print("Detect sp3 B center\n")
+        points = Points(subs_acid)
+        try:
+            plane = Plane.best_fit(points)
+            n1 = plane.normal
+            n2 = -n1
+            H_a1 = n1 * 1.2
+            H_a2 = n2 * 1.2
+            d1 = distance.euclidean(H_a1, coords[N_idx, :])
+            d2 = distance.euclidean(H_a2, coords[N_idx, :])
+            if d1 < d2:
+                vec_a = H_a1
+            else:
+                vec_a = H_a2
+        except ValueError:
+            if verb > 0:
+                print("Cannot determine the acid vector. return None")
+            return None, None, None, None
+    else:
+        print("Acid substituents are not 3!")
+        return None, None, None, None
+
+    vec_a = unit_vector(vec_a)
+    H_a = vec_a * 1.2 + coords[B_idx, :]
+
+    return H_a, H_b, subs_acid_idx, subs_base_idx
+
 
 if __name__ == '__main__':
     xyz1 = '/home/ppdeshmu/test_xyz.xyz'
--- src/iflp.py
+++ src/iflp.py
@@ -1,263 +1,16 @@
+import csv
 import logging
 import os
-from typing import List, Optional, Tuple
-
+from typing import Tuple, List
+import pandas as pd
 import numpy as np
+import submitit
+
 from rdkit import Chem
-from rdkit.Chem import AllChem, Get3DDistanceMatrix, rdmolfiles
-from scipy.spatial import distance
-from skspatial.objects import Plane, Points
+from rdkit.Chem import AllChem, rdmolfiles
 
-from calc_utils import angle_between, get_dft_E, unit_vector, xtb_opt_, get_lewis_h_dist
-
-
-def calculate_angle(
-    crd_B: int, crd_N: int, cm: np.ndarray, coords: np.ndarray, verb: int = 0
-) -> float:
-    """
-    Calculate the angle between two vectors for a given set of atoms.
-
-    Parameters:
-    - crd_B (int): Index of the boron atom.
-    - crd_N (int): Index of the nitrogen atom.
-    - cm (np.ndarray): Adjacency matrix.
-    - coords (np.ndarray): Atomic coordinates.
-    - verb (int): Verbosity level.
-
-    Returns:
-    float
-        The angle in degrees between two vectors, or 0.0 if the calculation cannot be performed.
-    """
-    subs_base = []
-    for n, j in enumerate(cm[crd_N, :]):
-        if j == 1:
-            pos = coords[n]
-            pos = np.around(pos, 4)
-            subs_base.append(pos)
-    if len(subs_base) == 2:
-        if verb > 1:
-            print("\nsp2 base. Calculating planes.")
-        subs_base.append(coords[crd_N, :])
-        points = Points(subs_base)
-        try:
-            centroid = points.centroid()
-        except ValueError:
-            if verb > 0:
-                print("Cannot calculate centroid. return 0.0")
-            return 0.0
-        vec_b = np.array(coords[crd_N, :]) - centroid
-
-    elif len(subs_base) == 3:
-        if verb > 1:
-            print("\nsp3 base. Calculating planes.")
-        points = Points(subs_base)
-        try:
-            centroid = points.centroid()
-        except ValueError:
-            if verb > 0:
-                print("Cannot calculate centroid. return 0.0")
-            return 0.0
-        vec_b = np.array(coords[crd_N, :]) - centroid
-    else:
-        print(
-            f"Base substituents are not 2 or 3 but rather {len(subs_base)} Angle set to 0!"
-        )
-
-    vec_b = unit_vector(vec_b)
-    subs_acid = []
-    for n, j in enumerate(cm[crd_B, :]):
-        if j == 1:
-            pos = coords[n]
-            pos = np.around(pos, 4)
-            subs_acid.append(pos)
-
-    if len(subs_acid) == 3:
-        if verb > 1:
-            print("\nsp3 acid. Calculating planes.")
-
-        points = Points(subs_acid)
-        try:
-            centroid = points.centroid()
-        except ValueError:
-            return 0.0
-        vec_a = np.array(coords[crd_B, :]) - centroid
-    else:
-        if verb > 0:
-            print("Acid substituents are not 3!")
-        return 0.0
-
-    vec_a = unit_vector(vec_a)
-
-    theta = angle_between(vec_a, vec_b) * 180 / np.pi
-    return theta
-
-
-def get_shortest_BN_distance(
-    mol: Chem.Mol, coords: np.ndarray, threshold: float
-) -> Tuple[float|None, int|None, int|None]:
-    """
-    Calculate the shortest B-N distance above a specified threshold.
-
-    Parameters:
-    - mol (Chem.Mol): The molecular structure.
-    - coords (np.ndarray): The coordinates of all atoms in the molecule.
-    - threshold (float): The threshold for considering B-N distances.
-
-    Returns:
-    tuple: A tuple containing the following:
-        - float or None: The shortest B-N distance greater than the threshold, or None if no such distance is found.
-        - int or None: The index of the boron (B) atom corresponding to the shortest distance, or None if not found.
-        - int or None: The index of the nitrogen (N) atom corresponding to the shortest distance, or None if not found.
-    """
-    B_dict = {}
-    N_dict = {}
-
-    for atom, c in zip(mol.GetAtoms(), coords):
-        if atom.GetAtomicNum() == 5:
-            B_dict[atom.GetIdx()] = c
-        elif atom.GetAtomicNum() == 7:
-            N_dict[atom.GetIdx()] = c
-
-    BN_dist_idx = {}
-
-    for bidx, bpos in B_dict.items():
-        for nidx, npos in N_dict.items():
-            dist = np.linalg.norm(bpos - npos)
-            if dist > threshold:
-                BN_dist_idx[dist] = (bidx, nidx)
-            else:
-                continue
-
-
-    min_dist = min(BN_dist_idx.keys())
-    if min_dist:
-        crd_B, crd_N = BN_dist_idx[min_dist]
-        return min_dist, crd_B, crd_N
-    else:
-        return None, None, None
-
-
-def get_H2_pos(
-        z: List[int],
-        crd_B: int,
-        crd_N: int,
-        cm: np.ndarray,
-        coords: np.ndarray,
-        verb: int = 0,
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Determine cartesian coodinates to which hydride and proton are to be added.
-
-    Vector definition on N: The centroid point of substituent groups - Coordinate of N
-    Vector definition on B: Coordinate of H(N) - Coordinate of B
-
-    Set bond distance:
-    d(B-H) = 1.2
-    d(N-H) = 1.2
-
-    Parameters:
-    1. z: vector of atomic numbers
-    2. crd_N: atomic index of N
-    3. crd_B: atomic index of B
-    4. cm: connectivity matrix
-    5. coords: coordinates matrix
-    6. verb: verbosity level
-
-    Returns:
-    H_a, H_b: coordinate of H(B) and H(N), respectively
-    """
-    # base vector definition
-    subs_base = [
-        np.around(coords[n], 4)
-        for n, j in enumerate(cm[crd_N, :])
-        if j == 1 and z[n] != 5
-    ]
-
-    if len(subs_base) == 2:
-        if verb > 1:
-            print("Detect sp2 nitrogen\n")
-        subs_base.append(coords[crd_N, :])
-        points = Points(subs_base)
-        try:
-            centroid = points.centroid()
-            vec_b = np.array(coords[crd_N, :]) - centroid
-        except ValueError:
-            if verb > 0:
-                print("Cannot calculate centroid. return None")
-            return None, None
-
-    elif len(subs_base) == 3:
-        if verb > 1:
-            print("Detect sp3 nitrogen\n")
-        points = Points(subs_base)
-
-        try:
-            plane = Plane.best_fit(points)
-            n1 = plane.normal
-            n2 = -n1
-            H_b1 = n1 * 1.2
-            H_b2 = n2 * 1.2
-            d1 = distance.euclidean(H_b1, coords[crd_B, :])
-            d2 = distance.euclidean(H_b2, coords[crd_B, :])
-            if d1 < d2:
-                vec_b = H_b1
-            else:
-                vec_b = H_b2
-
-
-        except ValueError:
-            if verb > 0:
-                print("Cannot determine the base vector. return None")
-            return None, None
-
-    else:
-        print(
-            f"Base substituents are not 2 or 3 but rather {len(subs_base)} return None"
-        )
-        return None, None
-
-    vec_b = unit_vector(vec_b)
-    H_b = vec_b * 1.2 + coords[crd_N, :]
-
-    # Acid vector definition
-    subs_acid = []
-    for n, j in enumerate(cm[crd_B, :]):
-        if j == 1:
-            # if the adjacent atom is not H(which will be the substrate
-            # atoms)
-            if z[n] != 1:
-                pos = coords[n]
-                pos = np.around(pos, 4)
-                subs_acid.append(pos)
-
-    if len(subs_acid) == 3:
-        if verb > 1:
-            print("Detect sp3 B center\n")
-        points = Points(subs_acid)
-        try:
-            plane = Plane.best_fit(points)
-            n1 = plane.normal
-            n2 = -n1
-            H_a1 = n1 * 1.2
-            H_a2 = n2 * 1.2
-            d1 = distance.euclidean(H_a1, coords[crd_N, :])
-            d2 = distance.euclidean(H_a2, coords[crd_N, :])
-            if d1 < d2:
-                vec_a = H_a1
-            else:
-                vec_a = H_a2
-        except ValueError:
-            if verb > 0:
-                print("Cannot determine the acid vector. return None")
-            return None, None
-    else:
-        print("Acid substituents are not 3!")
-        return None, None
-
-    vec_a = unit_vector(vec_a)
-    H_a = vec_a * 1.2 + coords[crd_B, :]
-
-    return H_a, H_b
+from ase.io import read
+from calc_utils import get_shortest_BN_distance, get_H2_pos, get_dft_E, xtb_opt_, get_lewis_h_dist, angle_between, calc_directed_vector
 
 
 class IFLP:
@@ -275,6 +28,10 @@ class IFLP:
         self.label = label
         self.save_folder = save_folder
         self.mol_relaxed_xyz = None
+        self.idx_dict = None
+
+        os.makedirs(os.path.join(save_folder, "initial_FLP"), exist_ok=True)
+        os.makedirs(os.path.join(save_folder, "xtb_optimized"), exist_ok=True)
 
 
     def calc_FEPA_FEHA(self, mol_FLP: Chem.Mol) -> Tuple[float, float]:
@@ -302,10 +59,10 @@ class IFLP:
 
         cm = Chem.rdmolops.GetAdjacencyMatrix(mol_FLP)
 
-        _, crd_B, crd_N = get_shortest_BN_distance(mol_FLP, coords, self.threshold)
+        _, B_idx, N_idx = get_shortest_BN_distance(mol_FLP, coords, self.threshold)
         z = [atom.GetAtomicNum() for atom in mol_FLP.GetAtoms()]
 
-        H_a, H_b = get_H2_pos(z, crd_B, crd_N, cm, coords, self.verb)
+        H_a, H_b, subs_acid_idx, subs_base_idx = get_H2_pos(z, B_idx, N_idx, cm, coords, self.verb)
         if (H_a is None) or (H_b is None):
             if self.verb > 0:
                 logging.error(
@@ -313,6 +70,13 @@ class IFLP:
                 )
             raise ValueError("Fail to generate H positions")
         else:
+            self.idx_dict = {
+                'B_idx': B_idx,
+                'N_idx': N_idx,
+                'subs_acid_idx': subs_acid_idx,
+                'subs_base_idx': subs_base_idx
+            }
+
             init_xyz_path = os.path.join(self.save_folder, f"initial_FLP/{self.label}.xyz")
             rdmolfiles.MolToXYZFile(mol_FLP, init_xyz_path)
 
@@ -350,7 +114,7 @@ class IFLP:
 
             opt_BH_path = xtb_opt_(flp_BH_path, self.save_folder, charge=-1)
 
-            d_bh = get_lewis_h_dist(opt_BH_path, crd_B)
+            d_bh = get_lewis_h_dist(opt_BH_path, B_idx)
 
             if d_bh > 1.5:
                 if self.verb > 0:
@@ -370,7 +134,7 @@ class IFLP:
 
             opt_NH_path = xtb_opt_(flp_NH_path, self.save_folder, charge=1)
 
-            d_nh = get_lewis_h_dist(opt_NH_path, crd_N)
+            d_nh = get_lewis_h_dist(opt_NH_path, N_idx)
             if d_nh > 1.5:
                 if self.verb > 0:
                     logging.error(
@@ -388,7 +152,7 @@ class IFLP:
             fepa, feha = 0, 0
         return fepa, feha
 
-    def geom_targets(self) -> List[float]:
+    def geom_targets(self) -> Tuple[float, float]:
         """
         Calculate geometric properties of a relaxed molecule.
 
@@ -400,16 +164,65 @@ class IFLP:
         If the calculation fails, an appropriate exception is raised.
         """
         try:
-            mol_relaxed = Chem.MolFromXYZFile(self.mol_relaxed_xyz)
-            cm = Chem.rdmolops.GetAdjacencyMatrix(mol_relaxed)
+            B_pos = self.idx_dict['B_idx']
+            N_pos = self.idx_dict['N_idx']
+            subs_acid_idx = self.idx_dict['subs_acid_idx']
+            subs_base_idx = self.idx_dict['subs_base_idx']
 
-            conf = mol_relaxed.GetConformer()
-            coords = conf.GetPositions()
+            atoms = read(self.mol_relaxed_xyz)
+            coords = atoms.get_positions()
 
-            best_dist, crd_B, crd_N = get_shortest_BN_distance(
-                mol_relaxed, coords, self.threshold
-            )
-            eta = calculate_angle(crd_B, crd_N, cm, coords, self.verb)
+            best_dist = np.linalg.norm(coords[B_pos] - coords[N_pos])
+
+            vec_acid = calc_directed_vector(B_pos, subs_acid_idx, coords)
+            vec_base = calc_directed_vector(N_pos, subs_base_idx, coords)
+
+            eta = angle_between(vec_acid, vec_base) * 180 / np.pi
             return best_dist, eta
+
         except Exception as e:
             raise ValueError(f"Fail to compute geometric properties: {str(e)}") from e
+
+def compute_desc(smiles, label):
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        iflp_calc = IFLP(label=label)
+
+        fepa, feha = iflp_calc.calc_FEPA_FEHA(mol)
+        d, phi = iflp_calc.geom_targets()
+
+        with open(desc_csv, 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow([label, fepa, feha, d, phi])
+        file.close()
+
+    except Exception as e:
+        print(f"Exception {e} in SMILES {smiles}")
+
+if __name__ == "__main__":
+    csv_path = "/home/ppdeshmu/Generative-FLP/data/csv/assembled_FLP.csv"
+    df = pd.read_csv(csv_path)
+
+    desc_csv = '/home/ppdeshmu/Generative-FLP/data/csv/FLP_dataset.csv'
+
+    header_row = ['label', 'fepa', 'feha', 'd', 'phi']
+    with open(desc_csv, 'w') as f:
+      writer = csv.writer(f)
+      writer.writerow(header_row)
+      f.close()
+
+    smi = df['SMILES'].to_list()
+
+    labels = range(len(smi))
+
+    executor = submitit.AutoExecutor(folder='/home/ppdeshmu/scratch')
+
+    if executor._executor.__class__.__name__ == 'SlurmExecutor':
+        executor.update_parameters(
+            cpus_per_task = 12,
+            tasks_per_node = 1,
+            slurm_nodes = 1,
+            slurm_time = "24:00:00"
+        )
+
+    jobs = executor.map_array(compute_desc, smi, labels)
